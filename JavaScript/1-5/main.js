17897eadc692a0a653ed20078537f442b8be90ed
jtkg2021 <81030373+jtkg2021@users.noreply.github.com>
2021-04-16 18:02:36 +0900

    課題提出
    1-5 関数

---
+++ JavaScript/1-5/main.js
@@ -0,0 +1,10 @@
+
+function createJuice(fruits) {
+  return fruits;
+}
+
+console.log(createJuice("みかん") + "を受け取りました。ジュースにして返します");
+
+let orangejuice = createJuice("みかん");
+console.log(orangejuice + "が届きました");
+
